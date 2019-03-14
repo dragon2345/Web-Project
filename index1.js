eb716dc13d4c55652db2f93b13f69d9ab06c79b4
= <=>
2019-03-14 10:23:38 +0900

    try to add js file

---
+++ index1.js
@@ -0,0 +1,12 @@
+// Viết thông báo dưới dạng document.write
+document.writeln("My name is Hưng <br>");
+document.writeln("Nice to meet you! <br>");
+
+// Viết thông báo dưới dạng console.log
+console.log("Tôi là Hưng");
+
+// Viết thông báo dưới dạng alert
+var n=10;
+
+var n=5;
+alert(n);
